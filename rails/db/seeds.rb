482e2c75f5269a775665cdb6729f6f26f23099ad
keita1899 <saboten18k@gmail.com>
2024-05-28 22:13:02 +0900

    userとadminの仮データを作成

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  last_name: "山田",
+  first_name: "太郎",
+  last_name_kana: "ヤマダ",
+  first_name_kana: "タロウ",
+  email: 'test@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+Admin.create!(
+  name: "admin",
+  password: "password",
+)
