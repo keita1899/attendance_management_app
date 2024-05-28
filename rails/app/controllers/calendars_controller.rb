a8f34b2ac95926b0600828eafa858c527ff254b1
keita1899 <saboten18k@gmail.com>
2024-05-28 22:15:42 +0900

    ログインしているユーザーだけカレンダーページに遷移できるように修正

--- rails/app/controllers/calendars_controller.rb
+++ rails/app/controllers/calendars_controller.rb
@@ -1,4 +1,12 @@
 class CalendarsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
+
+  private
+
+    def authenticate_user!
+      redirect_to new_user_session_path unless current_user
+    end
 end
