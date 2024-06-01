22379ee425766aa64aa1eb7d2241c714b7be1eae
keita1899 <saboten18k@gmail.com>
2024-06-01 20:57:01 +0900

    不必要なidを削除し、emailを一意なものに修正

--- rails/spec/factories/users.rb
+++ rails/spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :user do
-    id { 1 }
     last_name { "山田" }
     first_name { "太郎" }
     last_name_kana { "ヤマダ" }
     first_name_kana { "タロウ" }
-    email { "test@example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
