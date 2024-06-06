dc6aef00e66f0a86d074336bc900a9ec88b85626
keita1899 <saboten18k@gmail.com>
2024-06-06 22:28:18 +0900

    ユーザー情報の更新機能作成

--- rails/app/controllers/admins/users_controller.rb
+++ rails/app/controllers/admins/users_controller.rb
@@ -1,7 +1,31 @@
 class Admins::UsersController < Admins::BaseController
   layout "admin"
 
+  before_action :set_user, only: [:edit, :update]
+
   def index
     @users = User.page(params[:page]).order("created_at DESC")
   end
+
+  def edit
+    @wage = @user.wage || @user.build_wage
+  end
+
+  def update
+    if @user.wage.update(wage_params)
+      redirect_to admins_users_path, notice: "ユーザー情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.preload(:wage).find(params[:id])
+    end
+
+    def wage_params
+      params.require(:wage).permit(:weekday_rate, :weekend_rate)
+    end
 end
