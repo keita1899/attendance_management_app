d5b27d00ed6a81f31e791994a1cbb85c5b47cf4f
keita1899 <saboten18k@gmail.com>
2024-06-12 22:42:08 +0900

    rspecのファイル生成の設定を追加

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -36,5 +36,14 @@ module Myapp
     config.generators.system_tests = nil
 
     config.i18n.default_locale = :ja
+
+    config.generators do |g| 
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+      end
   end
 end
