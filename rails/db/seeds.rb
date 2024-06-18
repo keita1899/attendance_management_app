9c53edd789e94b87ac44cc554491bb63c786a024
keita1899 <saboten18k@gmail.com>
2024-06-18 11:08:52 +0900

    attendanceのダミーデータを定義

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -12,6 +12,21 @@
     weekday_hourly_wage: 1000,
     weekend_hourly_wage: 1100,
   )
+
+  user.create_attendance(
+    date: Date.today,
+    clock_in_time: Time.now.beginning_of_day + 10.hours,
+    clock_out_time: Time.now.beginning_of_day + 14.hours,
+    working_minutes: 240,
+    overtime_minutes: 0,
+    total_working_minutes: 240,
+    hourly_wage: 1000,
+    daily_wage: 4000,
+    transport_cost: 320,
+    allowance: 0,
+    total_payment: 4320,
+    special_day: false,
+  )
 end
 
 Admin.create!(
