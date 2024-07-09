49228cbf28f183a301bff7c00734210e75d111b1
keita1899 <saboten18k@gmail.com>
2024-07-09 13:31:56 +0900

    管理ダッシュボードの出勤一覧表示機能を作成

--- rails/app/controllers/admins/dashboard_controller.rb
+++ rails/app/controllers/admins/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admins::DashboardController < Admins::BaseController
   def index
+    @attendances = Attendance.preload(:user).where(date: Date.current).page(params[:page]).order("created_at DESC")
   end
 end
--- rails/app/decorators/attendance_decorator.rb
+++ rails/app/decorators/attendance_decorator.rb
@@ -17,4 +17,14 @@ class AttendanceDecorator < ApplicationDecorator
   def day_of_week(date)
     %w[日 月 火 水 木 金 土][date.wday]
   end
+
+  def status
+    if object.clock_in_time.nil?
+      '<span class="badge bg-secondary">未出勤</span>'.html_safe
+    elsif object.clock_out_time.nil?
+      '<span class="badge bg-success">出勤中</span>'.html_safe
+    else
+      '<span class="badge bg-dark">退勤済</span>'.html_safe
+    end
+  end
 end
